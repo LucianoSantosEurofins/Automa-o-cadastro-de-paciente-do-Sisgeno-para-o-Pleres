bf393182348a1623bdad5749f8c5cf8f417bd49f
Luciano Oliveira <D9LB@eurofinslatam.com>
2024-02-15 18:03:21 -0300

    Ler arquivo EXCEL no SISGENO

--- Automação cadastro de paciente do Sisgeno para o Pleres/ConversorXmlSisgenoParaXmlPleres.cs
+++ Automação cadastro de paciente do Sisgeno para o Pleres/ConversorXmlSisgenoParaXmlPleres.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
 {
     public class ConversorXmlSisgenoParaXmlPleres
     {
-        public Modelos_XML.XMLgenConectPleres ConverterXMLSisgeno_ParaXMLGenConect(Modelos_XML.XMLsisgeno xMLsisgeno)
+        public Modelos_XML.XMLgenConectPleres ConverterXMLSisgeno_ParaXMLGenConect(Modelos_XML.XMLsisgeno xMLsisgeno, string caminhoRelatorioSisgeno)
         {
+            var workbook = new XLWorkbook(caminhoRelatorioSisgeno);
+            var somenteLinhasPreenchidas = workbook.Worksheet(1).RowsUsed();
+
+            foreach (var row in somenteLinhasPreenchidas)
+            {
+                try
+                {
+                    var identificadorAmostra = row.Cell(1).Value.ToString();
+                    var NomePaciente = row.Cell(1).Value.ToString();
+                    var DataNacimento = row.Cell(1).Value.ToString();
+                    var DataSolicitacao = row.Cell(1).Value.ToString();
+                    //var identificadorAmostra = row.Cell(1).Value.ToString();
+                }
+                catch
+                {
+
+                }
+            }
+
+            xMLsisgeno.GetType();
             return null;
         }
     }
