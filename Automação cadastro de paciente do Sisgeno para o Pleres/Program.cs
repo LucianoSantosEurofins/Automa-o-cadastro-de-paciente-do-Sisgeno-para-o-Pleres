7afb7477d92d1bd52aa3fc0a7f09de310282077a
Luciano Oliveira <D9LB@eurofinslatam.com>
2024-02-22 17:45:30 -0300

    ajuste

--- Automação cadastro de paciente do Sisgeno para o Pleres/Program.cs
+++ Automação cadastro de paciente do Sisgeno para o Pleres/Program.cs
@@ -12,13 +12,11 @@ namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
         static void Main(string[] args)
         {
             var dataAtual = DateTime.Now;
-
             WebBot webBot = new WebBot();
             var linkGenConectPleres = ConfigurationManager.AppSettings["LinkGenConect"];
             var linkSisgeno =         ConfigurationManager.AppSettings["LinkSisgeno"];
             CriarPastaArquivoBaixadoDoSisgeno();
             webBot.BaixarXMLSisgeno_InserirXMLnoPleres(linkSisgeno, linkGenConectPleres, dataAtual);
-
             Console.ReadKey();
             Environment.Exit(0);
         }
