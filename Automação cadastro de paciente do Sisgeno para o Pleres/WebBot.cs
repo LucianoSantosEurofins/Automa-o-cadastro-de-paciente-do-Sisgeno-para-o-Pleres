ca240897ce959858c29a4008bfb07d3df2dbd8d6
Luciano Oliveira <D9LB@eurofinslatam.com>
2024-03-04 18:11:55 -0300

    Baixar arquivo Sisgeno (Em andamento) --Navegador deve baixar arquivo sem abrir caixa de pergunta do DownLoad

--- Automação cadastro de paciente do Sisgeno para o Pleres/WebBot.cs
+++ Automação cadastro de paciente do Sisgeno para o Pleres/WebBot.cs
@@ -14,7 +14,10 @@ namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
         public string BaixarXMLSisgeno_InserirXMLnoPleres(string sisgenoLink,string genConectLink, DateTime dataPacientes)
         {
             var options = new OpenQA.Selenium.Edge.EdgeOptions();
-            options.AddUserProfilePreference("download.default_directory", @"");
+            var downLoadPath = @"C:\Users\d9lb\OneDrive - Eurofins\Área de Trabalho\TesteArquivosSisgeno";
+            options.AddUserProfilePreference("download.default_directory", downLoadPath);
+            options.AddUserProfilePreference("download.prompt_for_download", false);
+            
             WebDriver webDriver = new OpenQA.Selenium.Edge.EdgeDriver(options);
             webDriver.Navigate().GoToUrl(sisgenoLink);
 
@@ -22,6 +25,8 @@ namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
             btnLaboratorio.Click();
             IWebElement txtCPF =         webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div[2]/form/div[1]/input"));
             IWebElement txtSenha =       webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div[2]/form/div[2]/input"));
+            IWebElement btnEntrar =       webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div[2]/form/div[4]/div/div/input"));
+            
             Thread.Sleep(2000);
             var cpf =   ConfigurationManager.AppSettings["cpfSisgeno"];
             var senha = ConfigurationManager.AppSettings["SenhaSisgeno"];
@@ -30,7 +35,27 @@ namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
             {
                 txtCPF.SendKeys(cpf);
                 txtSenha.SendKeys(senha);
-                //outras ações devem ser inseridas aqui
+                btnEntrar.Click();
+                Thread.Sleep(2000);
+                IWebElement dropDown = webDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div/div/div/select/option[2]"));
+                dropDown.Click();
+                //Thread.Sleep(1000);
+                //webDriver.FindElement(By.XPath("/html/body/form/table/tbody/tr[2]/td[1]/div[2]/table[2]/tbody/tr/td/ul/li[3]")).Click();
+                //Thread.Sleep(1000);
+                //webDriver.FindElement(By.XPath("/html/body/form/table/tbody/tr[2]/td[1]/div[2]/table[2]/tbody/tr/td/ul/li[3]/ul/li[2]/a")).Click(); 
+                webDriver.Navigate().GoToUrl("https://sisgeno.aids.gov.br/appRelatorio/frm_relatorio_situacao_exame.php");
+                
+                var txtInicio = webDriver.FindElement(By.XPath("/html/body/fieldset/div/div/form/div[2]/div[1]/input"));
+                var txtFim = webDriver.FindElement(By.Id("fim"));
+
+                txtFim.SendKeys(DateTime.Now.ToString("dd/MM/yyyy"));
+                txtInicio.SendKeys(DateTime.Now.ToString("dd/MM/yyyy"));
+
+                webDriver.FindElement(By.XPath("/html/body/fieldset/legend/h4/strong")).Click();
+                IWebElement btnEnviar = webDriver.FindElement(By.XPath("/html/body/fieldset/div/div/form/div[5]/div/input"));
+                btnEnviar.Click();
+                webDriver.SwitchTo().Alert().Accept();
+
             }
             catch (Exception ex)
             {
