8623fc7fe7bc3da4e21a198c12cf23359afe20cb
Luciano Oliveira <D9LB@eurofinslatam.com>
2024-03-26 17:18:31 -0300

    ;

--- Automação cadastro de paciente do Sisgeno para o Pleres/Program.cs
+++ Automação cadastro de paciente do Sisgeno para o Pleres/Program.cs
@@ -11,6 +11,7 @@ namespace Automação_cadastro_de_paciente_do_Sisgeno_para_o_Pleres
     {
         static void Main(string[] args)
         {
+            //teste commit
             var dataAtual = DateTime.Now;
             WebBot webBot = new WebBot();
             GerenciadorDeArquivos gerenciadorDeArquivos = new GerenciadorDeArquivos();
